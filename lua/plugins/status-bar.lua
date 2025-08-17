58e2c1b605ded9abccbbc09def4d151083e663fa
Andydandy21 <a.smithb21@gmail.com>
2025-08-17 10:55:16 -0500

    change filename tabline display

--- lua/plugins/status-bar.lua
+++ lua/plugins/status-bar.lua
@@ -7,6 +7,7 @@ return {
       vim.g.airline_powerline_fonts = 1
       vim.g["airline#extensions#tabline#enabled"] = 1
       vim.g.airline_theme = "rose_pine"
+      vim.g["airline#extensions#tabline#formatter"] = "unique_tail"
     end,
   },
 }
