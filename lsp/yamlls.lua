12f46bbae83a7b63ab60976d08894f0cbf11e916
Andrew Smith <a.smithb21@gmail.com>
2025-08-31 11:55:12 -0500

    update yaml config

--- lsp/yamlls.lua
+++ lsp/yamlls.lua
@@ -5,13 +5,20 @@ return {
 	settings = {
 		yaml = {
 			schemas = {
-				kubernetes = "/*.yaml",
+				["https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.28.0-standalone-strict/all.json"] = {
+					"k8s/**/*.yaml",
+					"kubernetes/**/*.yaml",
+					"manifests/**/*.yaml",
+					-- Exclude kustomization files
+					"!**/kustomization.yaml",
+					"!**/kustomization.yml",
+				},
 				["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
 				["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
 				["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
 				["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
 				["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
-				["https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/api/kustomization/v1beta1/kustomization.json"] = "kustomization.{yml,yaml}",
+				["https://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
 			},
 		},
 	},
--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -1,39 +1,39 @@
 vim.diagnostic.config({
-  virtual_text = { spacing = 2, prefix = "●" },
-  float = { border = "rounded" },
-  severity_sort = true,
+	virtual_text = { spacing = 2, prefix = "●" },
+	float = { border = "rounded" },
+	severity_sort = true,
 })
 
 vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(args)
-    local b = { buffer = args.buf, silent = true }
-    local map = vim.keymap.set
-    local lsp = vim.lsp.buf
-    -- general
-    map("n", "gd", lsp.definition, b)
-    map("n", "gD", lsp.declaration, b)
-    map("n", "gi", lsp.implementation, b)
-    map("n", "gr", lsp.references, b)
-    map("n", "K", lsp.hover, b)
-    map("n", "<leader>rn", lsp.rename, b)
-    map({ "n", "v" }, "<leader>ca", lsp.code_action, b)
+	callback = function(args)
+		local b = { buffer = args.buf, silent = true }
+		local map = vim.keymap.set
+		local lsp = vim.lsp.buf
+		-- general
+		map("n", "gd", lsp.definition, b)
+		map("n", "gD", lsp.declaration, b)
+		map("n", "gi", lsp.implementation, b)
+		map("n", "gr", lsp.references, b)
+		map("n", "K", lsp.hover, b)
+		map("n", "<leader>rn", lsp.rename, b)
+		map({ "n", "v" }, "<leader>ca", lsp.code_action, b)
 
-    -- diagnostics
-    map("n", "<C-w>d", vim.diagnostic.open_float, b)
-    map("n", "[d", function()
-      vim.diagnostic.jump({ count = -1, float = true })
-    end, { buffer = args.buf, desc = "Prev diagnostic" })
-    map("n", "]d", function()
-      vim.diagnostic.jump({ count = 1, float = true })
-    end, { buffer = args.buf, desc = "Next diagnostic" })
-  end,
+		-- diagnostics
+		map("n", "<C-w>d", vim.diagnostic.open_float, b)
+		map("n", "[d", function()
+			vim.diagnostic.jump({ count = -1, float = true })
+		end, { buffer = args.buf, desc = "Prev diagnostic" })
+		map("n", "]d", function()
+			vim.diagnostic.jump({ count = 1, float = true })
+		end, { buffer = args.buf, desc = "Next diagnostic" })
+	end,
 })
 
 vim.lsp.enable({
-  "lua_ls",
-  "ts_ls",
-  "rust-analyzer",
-  "bashls",
-  "yamlls",      
-  "helm_ls",    
+	"lua_ls",
+	"ts_ls",
+	"rust-analyzer",
+	"bashls",
+	"yamlls",
+	"helm_ls",
 })
