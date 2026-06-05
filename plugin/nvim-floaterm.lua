af9efc0f564495f6386725bc55ed25feee3614dd
Andydandy21 <a.smithb21@gmail.com>
2026-06-05 15:06:49 -0500

    upgrate floating terminal

---
+++ plugin/nvim-floaterm.lua
@@ -0,0 +1,14 @@
+local width = math.floor(vim.o.columns * 0.85)
+local height = math.floor(vim.o.lines * 0.85)
+
+vim.g.floaterm_width = width
+vim.g.floaterm_height = height
+vim.g.floaterm_borderchars = "─│─│╭╮╯╰"
+vim.g.floaterm_position = "top"
+vim.g.floaterm_autoclose = 1
+
+vim.keymap.set({ "n", "t" }, "<C-t>", "<cmd>FloatermToggle<CR>")
+vim.keymap.set({ "n", "t" }, "<C-[>", "<cmd>FloatermNext<CR>")
+vim.keymap.set({ "n", "t" }, "<C-]>", "<cmd>FloatermPrev<CR>")
+vim.keymap.set({ "n", "t" }, "<C-=>", "<cmd>FloatermNew<CR>")
+vim.keymap.set({ "n", "t" }, "<C-->", "<cmd>FloatermKill<CR>")
