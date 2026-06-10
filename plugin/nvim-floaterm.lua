94457ab52dfa9ad0b63bc55d8b25f02446fa0e24
Andydandy21 <a.smithb21@gmail.com>
2026-06-10 16:50:57 -0500

    fix terminal navigation bindings

--- plugin/nvim-floaterm.lua
+++ plugin/nvim-floaterm.lua
@@ -8,7 +8,7 @@ vim.g.floaterm_position = "top"
 vim.g.floaterm_autoclose = 1
 
 vim.keymap.set({ "n", "t" }, "<C-t>", "<cmd>FloatermToggle<CR>")
-vim.keymap.set({ "n", "t" }, "<C-[>", "<cmd>FloatermNext<CR>")
-vim.keymap.set({ "n", "t" }, "<C-]>", "<cmd>FloatermPrev<CR>")
+vim.keymap.set({ "n", "t" }, "<C-l>", "<cmd>FloatermNext<CR>")
+vim.keymap.set({ "n", "t" }, "<C-h>", "<cmd>FloatermPrev<CR>")
 vim.keymap.set({ "n", "t" }, "<C-=>", "<cmd>FloatermNew<CR>")
 vim.keymap.set({ "n", "t" }, "<C-->", "<cmd>FloatermKill<CR>")
